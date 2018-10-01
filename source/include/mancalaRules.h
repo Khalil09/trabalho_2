f29f30401d01def7e8c93eec2831285755030c10
Renato Avellar Nobre <rekanobre@gmail.com>
2018-10-01 17:57:58 -0300

    Adding Comments

--- source/include/mancalaRules.h
+++ source/include/mancalaRules.h
@@ -2,10 +2,17 @@
 #include <string.h>
 #include "mancalaUI.h"
 
+//
 void popular(int m_valores[][7]);
+//
 int m_vazia(int m_valores[][7]);
+//
 int referencia(char v);
+//
 void turno_p1(int m_valores[][7], int escolha, int jogada_AI);
+//
 int referencia2(char v);
+//
 void turno_p2(int m_valores[][7], int escolha, int jogada_AI);
+//
 void final(int m_valores[][7]);
